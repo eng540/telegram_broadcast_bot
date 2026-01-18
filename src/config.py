53b6fe41942f57237c199291b0e1849d8c0e8eaf
eng540 <132091714+eng540@users.noreply.github.com>
2026-01-18 19:18:05 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     MASTER_SOURCE_ID: int
     REDIS_URL: str
     DATABASE_URL: str
+    ALLOWED_LINK_SUBSTRING: str
+    ADMIN_ID: int = 0  # اختياري
 
     class Config:
         env_file = ".env"
